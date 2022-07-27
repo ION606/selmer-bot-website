60f5e5626cfd8a1d1c76e5a0bd3b40414c0e362c
ION606 <itamarorenn@protonmail.com>
2022-07-27 11:28:50 +0300

    Created the Premium page with Bootstrap and Stripe integration

--- main.js
+++ main.js
@@ -168,7 +168,7 @@ app.get('/', async ({ query }, response) => {
 					client_secret: clientSecret,
 					code,
 					grant_type: 'authorization_code',
-					redirect_uri: `http://localhost`,
+					redirect_uri: `https://selmer-bot-website.herokuapp.com`,
 					scope: 'identify',
 				}),
 				headers: {
